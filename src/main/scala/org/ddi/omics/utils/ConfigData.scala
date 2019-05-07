195ee1f2c42788af7d0d8aafa114ec54a67d51d4
Gaurhari <gaurharidass@gmail.com>
2019-05-07 11:09:35 +0100

    added function to retrieve data from configuration settings

---
+++ src/main/scala/org/ddi/omics/utils/ConfigData.scala
@@ -0,0 +1,13 @@
+package org.ddi.omics.utils
+
+import com.typesafe.config.{Config, ConfigFactory}
+
+object ConfigData {
+
+  def getConfig(filePath:String="application.conf"):Config = {
+    val conf = ConfigFactory.load(filePath)
+    conf
+  }
+
+}
+
