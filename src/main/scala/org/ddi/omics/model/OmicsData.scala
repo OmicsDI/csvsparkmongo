8602301f11ecf5ee922129e365118530ff8897b6
Gaurhari <gaurharidass@gmail.com>
2019-07-11 16:24:20 +0100

    csv fields model

--- src/main/scala/org/ddi/omics/model/OmicsData.scala
+++ src/main/scala/org/ddi/omics/model/OmicsData.scala
@@ -3,3 +3,12 @@ package org.ddi.omics.model
 class OmicsData {
 
 }
+
+case class CsvFields(accession:String, database:String, omicsType:Array[String],
+                     species:Array[String], tissue:Array[String], disease:Array[String], citationCount:Int,
+                     reanalysisCount:Int, searchDomain:Array[String], domainCount:Int,
+                     ensembl:Array[String],uniprot:Array[String],viewCount:Int,searchCount:Int,downloadCount:Int
+                    )
+
+case class Dataset(accession:String, database: String, connections:String,
+                   reanalysis:String = "", view:String = "", citation:String = "",download:String = "")
